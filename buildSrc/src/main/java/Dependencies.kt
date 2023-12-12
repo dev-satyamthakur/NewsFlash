0032bdcbc4a0e0f8328ab6028306c2dc64ac3b44
Satyam Thakur <sat.thakur20@gmail.com>
2023-12-12 21:01:34 +0530

    Creating a central dependency management buildSrc module

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,18 @@
+object Dependencies {
+
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val lifecycleRuntimeKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx}" }
+    val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
+    val composeBom by lazy { "androidx.compose:compose-bom:${Versions.composeBom}" }
+    val composeUi by lazy { "androidx.compose.ui:ui" }
+    val composeUiGraphics by lazy { "androidx.compose.ui:ui-graphics" }
+    val composeUiToolingPreview by lazy { "androidx.compose.ui:ui-tooling-preview" }
+    val composeMaterial3 by lazy { "androidx.compose.material3:material3" }
+    val junit by lazy { "junit:junit:${Versions.junit}" }
+    val junitExt by lazy { "androidx.test.ext:junit:1.1.5" }
+    val espressoCore by lazy { "androidx.test.espresso:espresso-core:${Versions.espressoCore}" }
+    val composeUiTestJunit4 by lazy { "androidx.compose.ui:ui-test-junit4" }
+    val composeUiTooling by lazy { "androidx.compose.ui:ui-tooling" }
+    val composeUiTestManifest by lazy { "androidx.compose.ui:ui-test-manifest" }
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+    const val coreKtx = "1.9.0"
+    const val lifecycleRuntimeKtx = "2.6.2"
+    const val activityCompose = "1.8.1"
+    const val composeBom = "2023.03.00"
+    const val junit = "4.13.2"
+    const val espressoCore = "3.5.1"
+}
